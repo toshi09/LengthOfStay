4c34d82f11243f905c04c1e7c26f6ccafd371147
Vikhyati singh <vikhyati@Vikhyatis-MacBook-Pro.local>
2016-04-04 01:28:11 -0700

    N fold

--- knn_train.py
+++ knn_train.py
@@ -1,6 +1,6 @@
-
 from sklearn.neighbors import KNeighborsClassifier
 import pickle
+import sklearn.cross_validation as cv
 
 def train_knn(nearest_neighbors, training_features, train_classes):
     # write your code here
@@ -10,12 +10,21 @@ def train_knn(nearest_neighbors, training_features, train_classes):
     neigh.fit(X, y)
     return neigh
 
-train_dict = pickle.load(open("/Users/oshpd/vikhyati/Desktop/training_data.pickle"))
-xx = train_dict['training_features']
-yy = train_dict['categories']
-model = train_knn(1000 , xx , yy)
+def cross_valid(xx, yy):
+    clf = KNeighborsClassifier(n_neighbors=1000)
+    scores = cv.cross_val_score(clf,xx,yy, cv=5)
+    print(scores)
 
 if __name__ == "__main__":
+    train_dict = pickle.load(open("/Users/vikhyati/Desktop/training_data.pickle"))
+    xx = train_dict['training_features']
+    yy = train_dict['categories']
+    cross_valid(xx, yy)
+    '''
+    model = train_knn(1000 , xx , yy)
+
     print model.score(xx ,yy )
+    print model.metrics.f1_score(xx,yy)
     print model.predict([xx[100]]) , yy[100]
+    '''
 # call train_knn function here
--- parse_data.py
+++ parse_data.py
@@ -5,6 +5,8 @@ import pickle
 
 def get_class(actual_next_los):
     actual_next_los = int (actual_next_los)
+    if actual_next_los == 0:
+        category = 0
     if actual_next_los == 1:
         category = 1
     elif actual_next_los == 2:
@@ -78,35 +80,61 @@ def featurize_row(row_data):
 
     return (patient_id,  patient_level_feature_vector + admission_level_feature_vector, category)
 
-def get_dummy_feature_seq(num_seq_reqd, feature_vector_length=17):
+def get_dummy_feature_seq(num_seq_reqd, feature_vector_length):
     '''
 
     :param num_seq_reqd:
     :param feature_vector_length:
     :return:
     '''
-    return [0.0 for xx in range(num_seq_reqd) for yy in feature_vector_length]
+    return [0.0  for xx in range(num_seq_reqd) for yy in range(feature_vector_length)]
 
-def read_oshpd_data(file_name):
+def read_oshpd_data(file_name, num_visit_seq_allowed):
     fhand = open(file_name)
     fhand.readline()
     XX = []
     YY = []
-
+    vist_no = 0
+    feature_vector_seq = []
+    YY = []
+    XX = []
+    prev_id = ''
+    curr_id = ''
     for line in fhand:
         row_data =  line.strip().split(',')
         out = featurize_row(row_data)
-        XX.append(out[1])
-        YY.append(out[2])
+        curr_id = out[0]
+        if prev_id != '' and curr_id != prev_id:
+            if vist_no > 0 and vist_no < num_visit_seq_allowed:
+                dummy_feature = get_dummy_feature_seq(num_visit_seq_allowed - vist_no, 54)
+                feature_vector_seq.extend(dummy_feature)
+                XX.append(feature_vector_seq)
+                YY.append(0)
+                feature_vector_seq =[]
+                vist_no = 0
 
-    train_dict = {'training_features':XX , 'categories' : YY}
+            feature_vector_seq.extend(out[1])
+            vist_no += 1
+        elif vist_no == num_visit_seq_allowed  - 1:
+            feature_vector_seq.extend(out[1])
+            XX.append(feature_vector_seq)
+            YY.append(out[2])
+            feature_vector_seq =[]
+            vist_no = 0
+        else:
+            feature_vector_seq.extend(out[1])
+            vist_no += 1
 
+        prev_id = curr_id
 
-    pickle.dump(train_dict, open("/Users/oshpd/vikhyati/Desktop/training_data.pickle", "wb"))
+    train_dict = {'training_features':XX, 'categories' : YY}
+
+    pickle.dump(train_dict, open('/Users/vikhyati/Desktop/seq_training_data.pickle', "wb"))
+
 
 if __name__ == "__main__":
-    file_name =  '/Users/oshpddata/Desktop/vikhyati/OSHPD_CLEAN_LOS.csv'  # Spec
-    read_oshpd_data(file_name)
+    file_name =  '/Users/vikhyati/Desktop/OSHPD_TEST.csv'  # Spec
+    read_oshpd_data(file_name, 3)
 
 
 
